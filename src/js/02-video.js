65e537c50e10209464df04fea0509c099d217c54
Anastasiia_Buhaienko <anastasiia.buhaienko@gmail.com>
2023-03-08 19:55:00 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
 
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (timeupdate) {  
+  localStorage.setItem("videoplayer-current-time", timeupdate.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedCurrentTime = localStorage.getItem("videoplayer-current-time");
+
+player.setCurrentTime(savedCurrentTime).then(function(seconds) {
+
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
+
+
+
