abefbb2c7579b41813fe2cdfc58f9e91d09310e3
Anastasiia_Buhaienko <anastasiia.buhaienko@gmail.com>
2023-03-11 15:45:12 +0200

    fix task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,10 @@
 
 import throttle from "lodash.throttle";
+import Player from "@vimeo/player";
+
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 const onPlay = function (timeupdate) {  
   localStorage.setItem("videoplayer-current-time", timeupdate.seconds);
